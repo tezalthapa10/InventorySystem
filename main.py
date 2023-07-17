8b07189b8c9add1456ddd7c43fb6a7dd8d54decb
Tezal Thapa <137768102+tezalthapa10@users.noreply.github.com>
2023-07-17 22:24:21 +0545

    First commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#using while loop so that program will not be terminated until user decised to
 while(True):
     #printing the welcome message for users
     print('''
@@ -15,22 +16,26 @@ while(True):
     #taking input from user for the options given
     choice = int(input("Enter your option to go forward:"))
     if choice == 1:
-        print("-"*70)
+        print("-"*70) #*70 will print 70 hypens(-)
         print('''
                 RENTAL SHOP KATHMANDU
         THE AVAILABLE STOCKS ARE AS FOLLOW''')
+        #Open the file in read mode and assign the file object to the variable 'data'
         data = open("equipment.txt","r")
+        #Read all lines from the file object and store them as a list of strings in the variable
         items = data.readlines()
-        print("-"*70)
+        print("-"*120)
         for item in items:
+            ##Split the string 'item' into four separate values based on the comma delimiter and assign them to variables
             Name,Brand, Price, Quantity = item.split(",")
-            print("{0}\t{1}\t{2}\t{3}".format(Name, Brand, Price, Quantity))
-        print("-"*70)
+            #Print formatted values with specified field widths
+            print("{0:30s}\t\t{1:25s}\t{2:10s}\t{3:10s}".format(Name, Brand, Price, Quantity))
+        print("-"*120)
     elif choice == 2:
         try:
             equipmentName = input("Enter equipment name: ")
             quantity=int(input("Enter quantity: "))
-            customerName=input("Enter customer name: ")    
+            customerName=input("Enter customer name: ")   
         except:
             print('Enter valid input ')
     else:
